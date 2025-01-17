e01cdfe63c657e420c108250fee253dfd079b567
Gregorio Galante <me@gregoriogalante.com>
2025-01-17 15:28:43 +0100

    Updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,14 @@
 const hre = require("hardhat");
 
+const UOMI_ENGINE_ADDRESS = "0x687528e4BC4040DC9ADBA05C1f00aE3633faa731";
 
 async function main() {
-
-  const uomiEngine = "0x687528e4BC4040DC9ADBA05C1f00aE3633faa731"
-  
-  const lock = await hre.ethers.deployContract("IPFSStorage", [uomiEngine]);
+  const lock = await hre.ethers.deployContract("IPFSStorage", [UOMI_ENGINE_ADDRESS]);
 
   await lock.deployed();
 
   console.log(`deployed to ${lock.address}`);
-
 }
-
 
 main().catch((error) => {
   console.error(error);
